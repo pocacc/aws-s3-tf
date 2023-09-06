7f363fd80ffcfb793fc2ac899a1cc41659c18e02
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-09-06 17:36:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "aws_s3" {
-  source = "git::https://github.com/pocacc/aws-modules/tree/main/aws_s3_bucket"
+  #source = "git::https://github.com/pocacc/aws-modules/tree/main/aws_s3_bucket"
+   source = "github.com/pocacc/aws-modules/tree/main/aws_s3_bucket"
+  
   bucket = var.bucket
 
 }
