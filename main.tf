7b3e028ba5a3ed35b0890e20c02779719407360e
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-11-09 15:46:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket
+  bucket = "ic-s3-deploy"
   force_destroy = var.force_destroy
   tags = {
-    Name        = var.bucket
-    Project     = "Poc_Telefonica_Backstage"
+    Name        = "ic-s3-deploy"
+    Project     = "ic-idp"
     Environment = "Development"
     E-mail      = var.email
   }
