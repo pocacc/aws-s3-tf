781ca7d8a05f980abcf58343dea616eb1db128cd
johnny-menezes <47942673+johnny-menezes@users.noreply.github.com>
2023-11-09 21:36:03 +0000

    ......

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "ic-s3-deploy"
+  bucket = "ic-s3-deploy-nndadjq"
   force_destroy = var.force_destroy
   tags = {
     Name        = "ic-s3-deploy"
@@ -9,16 +9,3 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.bucket]
-
-  bucket = aws_s3_bucket.bucket.id
-  acl    = var.acl
-}
