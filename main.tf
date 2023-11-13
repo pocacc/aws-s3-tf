aaf94b5d4f97daeb164c6fcdb92952713b911466
pocaccenture <143819793+pocaccenture@users.noreply.github.com>
2023-11-13 16:43:24 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "ic-s3-deploy-asdf-affa"
+  bucket = "ic-s3-deploy"
   force_destroy = var.force_destroy
   tags = {
     Name        = "ic-s3-deploy"
     Project     = "ic-idp"
     Environment = "Development"
-    E-mail      = "test-email"
+    E-mail      = var.email
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.acl
+}
