6244418703ed9ac8ca2ebe9ce2858fb30057dc96
pocaccenture <143819793+pocaccenture@users.noreply.github.com>
2023-11-14 11:00:43 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "ic-s3-deploy"
   force_destroy = var.force_destroy
   tags = {
-    Name        = "ic-s3-deploy"
+    Name        = var.bucket
     Project     = "ic-idp"
-    Environment = "Development"
+    Environment = var.environment
     E-mail      = var.email
   }
 }
