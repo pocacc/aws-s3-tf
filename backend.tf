59f927840fe003f2670ff07763c25a1ea0cbb947
pocaccenture <143819793+pocaccenture@users.noreply.github.com>
2023-11-14 15:38:04 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ic-github-tf-states"
-    key    = var.backend
-    region = var.region
+    key    = ${{ values.backend }}
+    region = ${{ values.region }}
   }
 }
